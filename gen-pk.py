54614cf3ec66b434cbef4b3126f09c7479513667
job306918 <128917451+job306918@users.noreply.github.com>
2024-01-01 08:40:00 +0800

    Create gen-pk.py

---
+++ gen-pk.py
@@ -0,0 +1,9 @@
+#
+import secrets
+from web3 import Web3
+
+for i in range(0,2):
+    pk = secrets.token_hex(32)
+    account = Web3().eth.account.privateKeyToAccount(pk)
+    print('Private key: %s' % pk)
+    print('Account adr: %s' % account.address)
